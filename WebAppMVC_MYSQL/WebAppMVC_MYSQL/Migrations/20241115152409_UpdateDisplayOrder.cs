efc455afa723cfa8757598d5844e5f0295dd4ee1
michelleog2351 <132501606+michelleog2351@users.noreply.github.com>
2024-11-16 20:55:33 +0000

    Add WebMVC_SQL directory to repo

---
+++ WebAppMVC_MYSQL/WebAppMVC_MYSQL/Migrations/20241115152409_UpdateDisplayOrder.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppMVC_MYSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDisplayOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "artsupply",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DisplayOrder",
+                value: 3333);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "artsupply",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DisplayOrder",
+                value: 1111);
+        }
+    }
+}
